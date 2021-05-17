ae8f7f83fc671a83bdf0e71667bfb24846dbdb20
ivol <ivol@criticalblue.com>
2021-05-17 10:43:38 +0100

    Typo

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ApproovURLSession",
-            exclude: ["README.md", "LICENSE"],
+            exclude: ["README.md", "LICENSE"]
             )
     ]
 )
