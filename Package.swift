b905a60266f614273f57661b8d8bd92b3a6b5d5d
ivol <ivol@criticalblue.com>
2021-05-17 23:59:06 +0100

    Typo

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         .library(
             name: "ApproovURLSession",
             targets: ["ApproovURLSession", "Approov"]),
-    ],,
+    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
