cc95225e1a0475bae4ee00d3037fb07b573280af
ivol <ivol@criticalblue.com>
2021-05-17 23:57:49 +0100

    Remove redundand dependency

--- Package.swift
+++ Package.swift
@@ -10,15 +10,7 @@ let package = Package(
         .library(
             name: "ApproovURLSession",
             targets: ["ApproovURLSession", "Approov"]),
-    ],
-    dependencies: [
-        // Here we define our package's external dependencies
-        // and from where they can be fetched:
-        .package(
-            url: "https://github.com/approov/approov-ios-sdk.git", 
-            .branch("master")
-        )
-    ],
+    ],,
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
