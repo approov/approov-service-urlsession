e24bf6f4072a91b236c361bbb31695e70edcd09e
ivo.liondov <ivol@Sanctuary.local>
2024-10-02 18:38:43 +0100

    Use common Approov framework for both ios and wathcos

--- Package.swift
+++ Package.swift
@@ -3,10 +3,8 @@ import PackageDescription
 
 // The release tag for the branch
 let releaseTAG = "3.2.3"
-// iOS SDK package version
-let sdkVersioniOS = "3.2.3"
-// watchOS SDK package version
-let sdkVersionWatchOS = "3.2.0"
+// SDK package version (used for both iOS and watchOS)
+let sdkVersion = "3.2.4"
 
 let package = Package(
     name: "ApproovURLSession",
@@ -15,42 +13,26 @@ let package = Package(
         .watchOS(.v7)
     ],
     products: [
-        // iOS Library
+        // Combined library for iOS and watchOS
         .library(
-            name: "ApproovURLSession-iOS",
-            targets: ["ApproovURLSession-iOS"]
-        ),
-        // watchOS Library
-        .library(
-            name: "ApproovURLSession-watchOS",
-            targets: ["ApproovURLSession-watchOS"]
+            name: "ApproovURLSession",
+            targets: ["ApproovURLSession"]
         )
     ],
     targets: [
-        // iOS Target
+        // Single target for both platforms
         .target(
-            name: "ApproovURLSession-iOS",
-            dependencies: ["Approov-iOS"],
-            path: "Sources/iOS",  // Set the custom source path for iOS
+            name: "ApproovURLSession",
+            dependencies: ["Approov"],
+            path: "Sources/ApproovURLSession",  // Point to the shared source code
             exclude: ["README.md", "LICENSE"]
         ),
+        // Binary target for the merged xcframework
         .binaryTarget(
-            name: "Approov-iOS",
-            url: "https://github.com/approov/approov-ios-sdk/releases/download/\(sdkVersioniOS)/Approov.xcframework.zip",
-            checksum: "8382b5ec920f8fbe7a41dd6b32a35a6289ed4a6a2ab7e2ed146ca4b669c8abf4"
-        ),
-        
-        // watchOS Target
-        .target(
-            name: "ApproovURLSession-watchOS",
-            dependencies: ["Approov-watchOS"],
-            path: "Sources/watchOS",  // Set the custom source path for watchOS
-            exclude: ["README.md", "LICENSE"]
-        ),
-        .binaryTarget(
-            name: "Approov-watchOS",
-            url: "https://github.com/approov/approov-watchos-sdk/releases/download/\(sdkVersionWatchOS)/Approov.xcframework.zip",
-            checksum: "124fea5b67eaba29985e0cc79884244a76fdbecc705a7d39c62ae97c21e137ac"
+            name: "Approov",
+            url: "https://github.com/approov/approov-ios-sdk/releases/download/\(sdkVersion)/Approov.xcframework.zip",
+            checksum: "0f332d1421fffe1072dfecc3616547d34484b4886008de6fd1db1a3c83881f99"
         )
     ]
 )
+
