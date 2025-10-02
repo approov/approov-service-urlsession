0059dab5b4c2001deb40acb430cf4fb2da522d29
ivo.liondov <ivol@Sanctuary.local>
2025-10-02 10:02:04 +0100

    Handle reinit of SDK with new return value: remove reduntant setter

--- Sources/ApproovURLSession/ApproovService.swift
+++ Sources/ApproovURLSession/ApproovService.swift
@@ -144,7 +144,6 @@ public class ApproovService {
                     let nsError = error as NSError
                     if nsError.code == 0, nsError.domain == "Foundation._GenericObjCError" {
                         os_log("ApproovService: Ignoring initialization error in Approov SDK: %@", type: .error, nsError.localizedDescription)
-                        isInitialized = true
                     } else {
                         throw ApproovError.initializationFailure(message: "Error initializing Approov SDK: \(nsError.localizedDescription)")
                     }
